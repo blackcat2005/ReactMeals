2ba4ed9445c126326d9750a3bd48ef2c0ab29a5f
Black Cat 2005 <ngaytanthe3579@gmail.com>
2022-03-09 22:47:11 +0700

    Header

---
+++ src/component/Layout/Header.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+
+import mealsMainImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+function Header(props) {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsMainImage} alt="A table full of food!" />
+      </div>
+    </Fragment>
+  );
+}
+
+export default Header;
