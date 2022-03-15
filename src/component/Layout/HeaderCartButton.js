ed092f025449036036f818c35174511a682e7f07
Black Cat 2005 <ngaytanthe3579@gmail.com>
2022-03-15 11:01:55 +0700

    Bump animation headerbtn

--- src/component/Layout/HeaderCartButton.js
+++ src/component/Layout/HeaderCartButton.js
@@ -1,18 +1,35 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import cartContext from "../../store/cart-context";
 
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(cartContext);
+  const [btnBump, setBtnBump] = useState(false);
 
-  const numberOfItems = cartCtx.items.reduce(
+  const { items } = cartCtx;
+  const numberOfItems = items.reduce(
     (curNumber, item) => curNumber + item.amount,
     0
   );
 
+  const btnClass = `${classes.button} ${btnBump ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnBump(true);
+    const timer = setTimeout(() => {
+      setBtnBump(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onShow}>
+    <button className={btnClass} onClick={props.onShow}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
